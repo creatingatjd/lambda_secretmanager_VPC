4d579df9fcc1a662e2715d6709461be392c856c2
creatingatjd <126667597+creatingatjd@users.noreply.github.com>
2023-10-16 20:53:45 +0100

    Update output.tf

--- module/vpc/output.tf
+++ module/vpc/output.tf
@@ -1,16 +1,8 @@
-#output "vpc_cidr"{
- #   description = "value of subnet1"
-  #  value = aws_vpc.demo-vpc
-#}
-output "subnet1_cidr"{
-    description = "value of subnet1"
-    value = aws_subnet.demo-subnet1.id
+output "subnet_ids" {
+    description = "security group id"
+    value = [aws_subnet.subnet1_public.id, aws_subnet.subnet2_public.id]
 }
-output "subnet2_cidr"{
-    description = "value of subnet1"
-    value = aws_subnet.demo-subnet2.id
-}
-output "sgname"{
-    description = "value of securitygroup"
-    value = aws_security_group.demo-sg.id
+output "sg" {
+    description = "security group id"
+    value = aws_default_security_group.default_security_group.id
 }
