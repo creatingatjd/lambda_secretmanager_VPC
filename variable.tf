a952985f620922fba8ef53a03db545c97a7667a5
creatingatjd <126667597+creatingatjd@users.noreply.github.com>
2023-10-16 20:58:31 +0100

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -37,23 +37,4 @@ variable "secretpolicy" {
   type        = string
   description = "This is the secret_key for aws account"
 }
-variable "vpc_cidr" {
-  type        = string
-  description = "This is the secret_key for aws account"
-}
-variable "subnet1_cidr" {
-  type        = string
-  description = "This is the secret_key for aws account"
-}
-variable "subnet2_cidr" {
-  type        = string
-  description = "This is the secret_key for aws account"
-}
-variable "sgname" {
-  type        = string
-  description = "This is the secret_key for aws account"
-}
-variable "rt_cidr" {
-  type        = string
-  description = "This is the secret_key for aws account"
-}
+
