9ddd08341cce2093ac5195ea2e8d73f4da0a24bd
creatingatjd <126667597+creatingatjd@users.noreply.github.com>
2023-10-25 10:09:10 +0100

    Update main.tf

--- module/vpc/main.tf
+++ module/vpc/main.tf
@@ -1,136 +1,132 @@
-# VPC
+resource "aws_vpc" "demo-vpc" { 
 
-resource "aws_vpc" "vpc" {
-  cidr_block           = "10.0.0.0/16"
-    tags = {
-    Name = "myapp-vpc"
-  }
-}
+  cidr_block = "10.0.0.0/16"
 
-# Subnets
-# - Public
-resource "aws_subnet" "subnet1_public" {
-  vpc_id                  = aws_vpc.vpc.id
-  availability_zone       = "eu-west-2a"
-  cidr_block              = "10.0.0.0/21"
-  map_public_ip_on_launch = true
-  tags = {
-    Name = "myapp-subnet1-public"
-  }
-}
-resource "aws_subnet" "subnet2_public" {
-  vpc_id                  = aws_vpc.vpc.id
-  availability_zone       = "eu-west-2b"
-  cidr_block              = "10.0.16.0/21"
-  map_public_ip_on_launch = true
-  tags = {
-    Name = "myapp-subnet2-publc"
-  }
-}
+} 
 
+//create subnet
+resource "aws_subnet" "demo-subnet1" { 
 
-# Internet gateway
-resource "aws_internet_gateway" "internet_gateway" {
-  vpc_id = aws_vpc.vpc.id
+  vpc_id     = aws_vpc.demo-vpc.id
+
+  cidr_block = var.demo-subnet1
 
   tags = {
-    Name = "myapp-igw"
-  }
-}
 
-# Routes tables
-# - Public
-resource "aws_route_table" "route_table1_public" {
-  vpc_id = aws_vpc.vpc.id
+    Name = "demo-subnet"
+
+  }
+
+} 
+resource "aws_subnet" "demo-subnet2" { 
+
+  vpc_id     = aws_vpc.demo-vpc.id
+
+  cidr_block = var.demo-subnet2
+
+  tags = {
+
+    Name = "demo-subnet"
+
+  }
+
+} 
+
+//create internet gateway
+
+resource "aws_internet_gateway" "demo-ig" {
+
+  vpc_id = aws_vpc.demo-vpc.id
+
+  tags = {
+
+    Name = "demo-ig"
+
+  }
+
+}
+//create a route table
+
+resource "aws_route_table" "demo-rt" { 
+
+  vpc_id = aws_vpc.demo-vpc.id
 
   route {
+
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.internet_gateway.id
+
+    # gateway_id = aws_internet_gateway.demo-ig.id
+    gateway_id = aws_nat_gateway.nat.id
+
   }
 
-  tags = {
-    Name = "myapp-rtb-public"
+    tags = {
+
+    Name = "demo-rt"
+
   }
+
+} 
+
+//create a subnet association
+
+resource "aws_route_table_association" "demo-rt-association1" {
+
+  subnet_id = aws_subnet.demo-subnet1.id
+  route_table_id = aws_route_table.demo-rt.id
+
+} 
+resource "aws_route_table_association" "demo-rt-association2" {
+
+  subnet_id = aws_subnet.demo-subnet2.id
+  route_table_id = aws_route_table.demo-rt.id
+
+} 
+
+//create security groups
+
+resource "aws_security_group" "demo-sg" {
+
+   name = "demo-sg"
+   description = "Allow TLS inbound traffic"
+   vpc_id = aws_vpc.demo-vpc.id
+   ingress {
+     description = "TLS from VPC"
+     from_port = 22
+     to_port = 22
+     protocol = "tcp"
+     cidr_blocks = ["0.0.0.0/0"]
+  } 
+   ingress {
+     description = "TLS from VPC" 
+     from_port = 80 
+     to_port = 80
+     protocol = "tcp" 
+     cidr_blocks = ["0.0.0.0/0"]       
+  } 
+   ingress {
+     description = "TLS from VPC"  
+     from_port = 443
+     to_port = 443
+     protocol = "tcp" 
+     cidr_blocks = ["0.0.0.0/0"]  
+}   
+   egress {
+      from_port = 0
+      to_port = 0
+      protocol = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+   }
 }
 
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.eip.id
+  subnet_id     = aws_subnet.demo-subnet1.id
 
-resource "aws_route_table_association" "route_table_association1_public" {
-  subnet_id      = aws_subnet.subnet1_public.id
-  route_table_id = aws_route_table.route_table1_public.id
+  # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway for the VPC.
+  depends_on = [aws_internet_gateway.demo-ig]
 }
-resource "aws_route_table_association" "route_table_association2_public" {
-  subnet_id      = aws_subnet.subnet2_public.id
-  route_table_id = aws_route_table.route_table1_public.id
+resource "aws_eip" "eip" {
+  domain   = "vpc"
 }
-
-
-resource "aws_eip" "eip1" {
-  vpc        = true
-  depends_on = [aws_internet_gateway.internet_gateway]
-  tags = {
-    Name = "myapp-eip1"
-  }
-}
-
-
-
-resource "aws_nat_gateway" "nat_gateway1" {
-  allocation_id = aws_eip.eip1.id
-  subnet_id     = aws_subnet.subnet1_public.id
-
-  tags = {
-    Name = "myapp-ngw1"
-  }
-}
-
-resource "aws_default_network_acl" "default_network_acl" {
-  default_network_acl_id = aws_vpc.vpc.default_network_acl_id
-  subnet_ids             = [aws_subnet.subnet1_public.id, aws_subnet.subnet2_public.id]
-
-  ingress {
-    protocol   = -1
-    rule_no    = 100
-    action     = "allow"
-    cidr_block = "0.0.0.0/0"
-    from_port  = 0
-    to_port    = 0
-  }
-
-  egress {
-    protocol   = -1
-    rule_no    = 100
-    action     = "allow"
-    cidr_block = "0.0.0.0/0"
-    from_port  = 0
-    to_port    = 0
-  }
-
-  tags = {
-    Name = "myapp-default-network-acl"
-  }
-}
-
-
-resource "aws_default_security_group" "default_security_group" {
-  vpc_id = aws_vpc.vpc.id
-
-  ingress {
-    from_port = 0
-    to_port   = 0
-    protocol  = -1
-    # self      = true
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "myapp-default-sg"
-  }
-}
-
-
