36376952c609af347d8a4bab7d687777eeb2e15e
creatingatjd <126667597+creatingatjd@users.noreply.github.com>
2023-10-25 10:10:47 +0100

    Update variables.tf

--- module/vpc/variables.tf
+++ module/vpc/variables.tf
@@ -1,1 +1,11 @@
+variable "demo-subnet1" {
+  type        = string
+  description = "public subnet cidr for az2"
+  #default     = "10.0.0.0/24"
+}
 
+variable "demo-subnet2" {
+  type        = string
+  description = "public subnet cidr for az2"
+  #default     = "10.10.1.0/24"
+}
