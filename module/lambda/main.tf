3d04547f937d7d586cef852409c91f3b2ca9ac58
creatingatjd <126667597+creatingatjd@users.noreply.github.com>
2023-10-25 10:07:47 +0100

    Update main.tf

--- module/lambda/main.tf
+++ module/lambda/main.tf
@@ -1,32 +1,26 @@
 # creating iam role
-resource "aws_iam_role" "lambda_role" {
-  name               = var.aws_iam_role
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Sid    = ""
-        Principal = {
-          Service = "lambda.amazonaws.com"
-        }
-      },
-    ]
-  })
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
 
-  tags = {
-    tag-key = "tag-value1"
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
   }
 }
 
-data "aws_iam_policy" "example" {
-  name = var.secretpolicy
+resource "aws_iam_role" "lambda_role" {
+  name               = var.lambda_role
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+  
 }
 
 #creating iam policy
 resource "aws_iam_policy" "lambda_policy" {
-  name        = var.aws_iam_policy
+  name        = var.lambda_policy
   path        = "/"
   description = "IAM policy for logging from a lambda"
   policy = jsonencode({
@@ -45,45 +39,66 @@ resource "aws_iam_policy" "lambda_policy" {
     ]
   })
 }
-#creating role_polciy_attachment
-resource "aws_iam_role_policy_attachment" "lambda_policy_role_attachment1" {
-  role       = aws_iam_role.lambda_role.name
-  policy_arn = aws_iam_policy.lambda_policy.arn
+
+resource "aws_iam_policy" "secret_policy" {
+  name       = var.secret_policy # Add a name for your policy
+  //secret_arn = aws_secretsmanager_secret.mongodb_secret.arn
+  policy     = jsonencode({
+    Version  = "2012-10-17",
+    Statement = [
+      {
+        Sid       = "Stmt1696495460910",
+        Action    = "secretsmanager:*",
+        Effect    = "Allow",
+        Resource  = "*"
+      },
+    ],
+  })
 }
-resource "aws_iam_role_policy_attachment" "lambda_policy_role_attachment2" {
-  role       = aws_iam_role.lambda_role.name
-  policy_arn = data.aws_iam_policy.example.arn
-}
+
 resource "aws_iam_role_policy_attachment" "iam_role_policy_attachment_lambda_vpc_access_execution" {
   role       = aws_iam_role.lambda_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
+}
+#creating role_polciy_attachment
+resource "aws_iam_role_policy_attachment" "lambda_policy_role_attachment" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.lambda_policy.arn
+}
+resource "aws_iam_role_policy_attachment" "secret_policy_role_attachment" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.secret_policy.arn
+}
+data "aws_secretsmanager_secret" "examplesecret" {
+  name = var.secret_name
 }
 
 #zipping python file 
 data "archive_file" "lambda_archive_file" {
   type        = "zip"
   source_dir  = "${path.module}/python/"
-  output_path = "${path.module}/python/hello.zip"
+  output_path = "${path.module}/python/lambda_handler.zip"
 }
 
 #creating lambda function
 resource "aws_lambda_function" "lambda_function" {
   # If the file is not in the current working directory you will need to include a
   # path.module in the filename.
-  filename      = "${path.module}/python/hello.zip"
-  function_name = var.aws_lambda_function
+  filename      = "${path.module}/python/lambda_handler.zip"
+  function_name = var.lambda_function
   role          = aws_iam_role.lambda_role.arn
-  handler       = var.aws_lambda_function_handler
+  handler       = var.lambda_function_handler
   source_code_hash = data.archive_file.lambda_archive_file.output_base64sha256
-  runtime = "python3.9"
+  runtime = "python3.10"
   timeout = 10
-  depends_on = [aws_iam_role_policy_attachment.lambda_policy_role_attachment1, aws_iam_role_policy_attachment.lambda_policy_role_attachment2, aws_iam_role_policy_attachment.iam_role_policy_attachment_lambda_vpc_access_execution ]
+  depends_on = [aws_iam_role_policy_attachment.lambda_policy_role_attachment, aws_iam_role_policy_attachment.secret_policy_role_attachment, aws_iam_role_policy_attachment.iam_role_policy_attachment_lambda_vpc_access_execution]
   vpc_config {
-   subnet_ids         = [var.demo-subnet1] # Use the subnet ID(s) you defined
-   security_group_ids = [var.security_group_id]1cify your security group(s)
+  subnet_ids         = [var.subnet1-id, var.subnet2-id] # Use the subnet ID(s) you defined
+  security_group_ids = [var.security_group_id] # Specify your security group(s)
   }
-   environment {
-    variables = {
-      SECRET_NAME= var.secretname
+  environment {
+  variables = {
+    SECRET_NAME= var.secret_name
+  }
   }
 }
